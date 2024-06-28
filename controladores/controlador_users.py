d1eebcfd3da75777cacc16d9595dd12e5d879e1c
Marco Rioja <77291154@usat.pe>
2024-06-28 10:39:51 -0500

    saas

--- controladores/controlador_users.py
+++ controladores/controlador_users.py
@@ -1,38 +1,37 @@
 from bd import obtener_conexion
-import hashlib
-import random
 
-def registrar_usuario(username, password):
-    password_hash = hashlib.sha256(password.encode()).hexdigest()
-    codigo_verificacion = random.randint(100000, 999999)
-    conexion = obtener_conexion()
-    with conexion.cursor() as cursor:
-        cursor.execute(
-            "INSERT INTO usuarios(usuario, password_hash, codigo_verificacion, estado_verificado) VALUES (%s, %s, %s, %s)",
-            (username, password_hash, codigo_verificacion, False))
-    conexion.commit()
-    conexion.close()
-    return codigo_verificacion
-
-def confirmar_usuario(username, codigo_verificacion):
-    conexion = obtener_conexion()
-    verificado = False
-    with conexion.cursor() as cursor:
-        cursor.execute(
-            "UPDATE usuarios SET estado_verificado = %s WHERE usuario = %s AND codigo_verificacion = %s",
-            (True, username, codigo_verificacion))
-        if cursor.rowcount == 1:
-            verificado = True
-    conexion.commit()
-    conexion.close()
-    return verificado
-
-def obtener_usuario_por_username(username):
+def obtener_user_por_username(username):
     conexion = obtener_conexion()
     user = None
     with conexion.cursor() as cursor:
         cursor.execute(
-            "SELECT usuario, password_hash, codigo_verificacion, estado_verificado FROM usuarios WHERE usuario = %s", (username,))
+            "SELECT id, email, password, token FROM users WHERE email = %s", (username,))
         user = cursor.fetchone()
     conexion.close()
     return user
+
+def obtener_user_por_id(id):
+    conexion = obtener_conexion()
+    user = None
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "SELECT id, email, password FROM users WHERE id = %s", (id,))
+        user = cursor.fetchone()
+    conexion.close()
+    return user
+
+def insertar_user(username, epassword):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("INSERT INTO users(email, password) VALUES (%s, %s)",
+                       (username, epassword))
+    conexion.commit()
+    conexion.close()
+
+def actualizartoken_user(username, token):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("UPDATE users SET token =%s WHERE email = %s ",
+                       (token, username))
+    conexion.commit()
+    conexion.close()
